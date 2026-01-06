8e4d8f184569d43ef2dec6485006ee9cf363a9a6
saravg2012@gmail.com <saravg2012@gmail.com>
2026-01-05 21:31:09 -0600

    first-commit

--- src/main/java/com/example/awscicd/SpringbootCicdDemoApplication.java
+++ src/main/java/com/example/awscicd/SpringbootCicdDemoApplication.java
@@ -2,13 +2,21 @@ package com.example.awscicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootCicdDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCicdDemoApplication.class, args);
 		System.out.println("Helllllooooo");
+	}
+
+	@GetMapping("/getData")
+	public String getData() {
+		return "Welcome to aws cicd process using aws codepipeline and codebuild";
 	}
 
 }
