9019f718d7c429af4f6b8409d26c81c430d79d75
saravg2012@gmail.com <saravg2012@gmail.com>
2026-01-05 21:19:07 -0600

    first-commit

---
+++ src/test/java/com/example/awscicd/SpringbootCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
